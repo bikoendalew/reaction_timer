1fe60066b625f48e6676c30c7bf90a2e7e6762b9
biruk <birukendalew090@gmail.com>
2024-03-21 21:42:10 +0300

    return result

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
   <h1>Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="this.delay"/>
+  <Block v-if="isPlaying" :delay="this.delay" @end="endGame"/>
+  
+  <Result :score="score" v-if="showResult"/>
 </template>
 
 <script>
 
 import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 export default {
   name: 'App',
   components: {
-   Block
+   Block,Result
   },
   data(){
     return{
       isPlaying:false,
-      delay:null
+      delay:null,
+      score:0,
+      showResult:false
     }
   },
   methods:{
+    endGame(getValue){
+      this.showResult=true
+      this.score=getValue
+      this.isPlaying = false
+    },
     start(){
+      this.showResult=false
       this.isPlaying = true,
       this.delay=2000+Math.random() * 5000
     }
@@ -35,5 +46,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button{
+  background:  #0faf78;
+  color:white;
+  border:none;
+  padding:8px 16px;
+  border-radius: 4px;
+  font-size:16px;
+  letter-spacing: 1px;
+  cursor:pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
